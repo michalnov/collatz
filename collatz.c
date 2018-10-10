119848d562b1087fd0a8129e75c3116c22f3310f
michal.novotny <michal.novotny@akademiasovy.sk>
2018-10-10 23:55:42 +0200

    initial commit

---
+++ collatz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int num = 1;
+  int count = 0; //Use only for task
+
+  /*Read input from user into variable num*/
+
+  /*Input validation*/
+
+  while (num != 1) {  //main cycle
+    /* code */
+    if (num % 2 == 0) {
+      /* code */
+    } else {
+      /* code */
+    }
+
+  }
+  return 0;
+}
