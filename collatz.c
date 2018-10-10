0c2d559fcbcd542d276a78578807c33af89aafa8
michalnov <32653270+michalnov@users.noreply.github.com>
2018-10-11 00:26:09 +0200

    Update collatz.c

--- collatz.c
+++ collatz.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[]) {
   int num = 1;
-  int count = 0; //Use only for task
+  int count = 0; //Use only for task 4.1, 4.2
 
   /*Read input from user into variable num*/
 
