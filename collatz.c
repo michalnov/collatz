53d273cad1b06b3ee89a01a1b10db8207ebc2d7f
michalnov <32653270+michalnov@users.noreply.github.com>
2018-10-11 11:30:00 +0200

    Update collatz.c

--- collatz.c
+++ collatz.c
@@ -5,8 +5,13 @@ int main(int argc, char const *argv[]) {
   int count = 0; //Use only for task 4.1, 4.2
 
   /*Read input from user into variable num*/
+  scanf(%d, &num);
 
   /*Input validation*/
+  
+ if(num < 1 || num > 1000){
+  return 0;
+ }
 
   while (num != 1) {  //main cycle
     /* code */
